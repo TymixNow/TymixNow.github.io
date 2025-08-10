0d4f5b871a6f52db86bc6185dd93a0bece1179c5
TymixNow <tymixnow@gmail.com>
2025-08-10 21:47:57 +0200

    2025/08/10

--- haml/data.rb
+++ haml/data.rb
@@ -28,20 +28,42 @@ $nav = [
     },
 ]
 
+$codes = {
+    gen: {code: "GEN", expl: "GENeral"},
+    web: {code: "CEW", expl: "Current Endeavour Website"},
+    chip: {code: "SCH", expl: "Sound CHip"},
+    tcr: {code: "WBL:TCR", expl: "TriConsonantal Roots"},
+    wb: {code: "WBL", expl: "WorldBuiLding"},
+    game: {code: "HCG", expl: "Hexagonal Cogwheel Game"}
+}
+
 $contents = [
-  {
-    codename: "GEN-01",
-    code_expl: "GENeral",
-    title: "VM",
-    img_alt: "A screenshot from a virtual machine",
-    date:
     {
-      year: 2025,
-      month: 03,
-      day: 18,
+        code: :gen,
+        index: 2,
+        title: "I'm back (again)",
+        img_alt: "piece of code used for this website",
+        date:
+        {
+            year: 2025,
+            month: 8,
+            day: 10
+        },
+        post_desc: "I forgot about this place for a while"
     },
-    post_desc: "Virtual Machine",
-  },
+    {
+        codename: "GEN-01",
+        code_expl: "GENeral",
+        title: "VM",
+        img_alt: "A screenshot from a virtual machine",
+        date:
+        {
+        year: 2025,
+        month: 03,
+        day: 18,
+        },
+        post_desc: "Virtual Machine",
+    },
     {
         codename: "CEW-02",
         code_expl: "Current Endeavour Website",
@@ -57,7 +79,7 @@ $contents = [
     },
     {
         codename: "SCH-01",
-        code_expl: "Sound Chip",
+        code_expl: "Sound CHip",
         title: "Making Music Chips with Meehow",
         img_alt: "a few header files used in the project",
         date:
@@ -107,49 +129,53 @@ $contents = [
         },
         post_desc: "Return of (and to) the conlang."
     },
-  {
-      codename: "HCG-01",
-      code_expl: "Hexagonal Cogwheel Game",
-      title: "Cogwheel Game Prototype",
-      img_alt: "Blank Hex",
-      date:
-      {
-          year: 2024,
-          month: 12,
-          day: 11
-      },
-      post_desc: "A prototype for a game cogwheel system."
-  },
-  {
-      codename: "TCR-01",
-      code_expl: "Tri-Consonantal Roots",
-      title: "TCR-lang",
-      img_alt: "Piece of an IPA table",
-      date:
-      {
-          year: 2024,
-          month: 12,
-          day: 5
-      },
-      post_desc: "A conlang with plans to make it have Tri-consonantal roots."
-  },
-  {
-      codename: "CEW-01",
-      code_expl: "Current Endeavour Website",
-      title: "This Website",
-      img_alt: "image of a line of HTML code",
-      date:
-      {
-          year: 2024,
-          month: 10,
-          day: 11
-      },
-      post_desc: "Just a place to practise HTML while organising ideas."
-  }
+    {
+        codename: "HCG-01",
+        code_expl: "Hexagonal Cogwheel Game",
+        title: "Cogwheel Game Prototype",
+        img_alt: "Blank Hex",
+        date:
+        {
+            year: 2024,
+            month: 12,
+            day: 11
+        },
+        post_desc: "A prototype for a game cogwheel system."
+    },
+    {
+        codename: "TCR-01",
+        code_expl: "Tri-Consonantal Roots",
+        title: "TCR-lang",
+        img_alt: "Piece of an IPA table",
+        date:
+        {
+            year: 2024,
+            month: 12,
+            day: 5
+        },
+        post_desc: "A conlang with plans to make it have Tri-consonantal roots."
+    },
+    {
+        codename: "CEW-01",
+        code_expl: "Current Endeavour Website",
+        title: "This Website",
+        img_alt: "image of a line of HTML code",
+        date:
+        {
+            year: 2024,
+            month: 10,
+            day: 11
+        },
+        post_desc: "Just a place to practise HTML while organising ideas."
+    }
 ]
 $contents.each do |content|
-  content[:mm] = '%02d' % content[:date][:month].to_s
-  content[:dd] = '%02d' % content[:date][:day].to_s
-  content[:date_link] = "#{content[:date][:year]}/#{content[:mm]}/#{content[:dd]}"
-  content[:date] = "#{content[:dd]}.#{content[:mm]}.#{content[:date][:year]}"
+    content[:mm] = '%02d' % content[:date][:month].to_s
+    content[:dd] = '%02d' % content[:date][:day].to_s
+    content[:date_link] = "#{content[:date][:year]}/#{content[:mm]}/#{content[:dd]}"
+    content[:date] = "#{content[:dd]}.#{content[:mm]}.#{content[:date][:year]}"
+    unless content[:code] == nil then
+        content[:codename] = "#{$codes[content[:code]][:code]}-#{'%02d' % content[:index].to_s}"
+        content[:codename] = $codes[content[:code]][:expl]
+    end
 end
